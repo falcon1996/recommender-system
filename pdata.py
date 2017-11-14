60fba6ffa6545085df60919df464303dd088a0ff
falcon <dhruvparashar6@gmail.com>
2017-11-14 22:39:03 +0530

    Linked Dataframe with Sframe

---
+++ pdata.py
@@ -0,0 +1,44 @@
+import matplotlib as mpl
+mpl.use('Agg')
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+import graphlab
+from graphlab import SFrame
+from graphlab.toolkits.recommender import ranking_factorization_recommender
+
+col_names = ["user_id", "item_id", "rating", "timestamp"]
+data = pd.read_table('./u.data', names=col_names)
+data = data.drop('timestamp', 1)
+data.info()
+pdf = pd.DataFrame(data)
+
+sf = SFrame(pdf)
+
+"""
+PopularityRecommender methods
+"""
+
+myrating = ranking_factorization_recommender.create(sf, target='rating')
+
+
+train, test = train_test_split(pdf, test_size = 0.3)
+
+training,validation = train_test_split(train, test_size = 0.25)
+
+Number_Ratings = len(data)
+Number_Movies = len(np.unique(data['item_id']))
+Number_Users = len(np.unique(data['user_id']))
+Sparcity = (Number_Ratings/(Number_Movies*Number_Users))*100
+print('Number_Users:',Number_Users, 'Number_Movies:',Number_Movies, 'Number_Ratings:',Number_Ratings)
+print('Sparcity(%)', Sparcity)
+
+#print(pdf)
+print(len(train), len(test), len(training), len(validation))
+#print(sf)
+print(myrating)
+
+plt.hist(data['rating'])
+plt.show()
+plt.savefig('myfig')
